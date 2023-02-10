39737d942f723f59dbb9d10f6dde10fe4aeb7e08
Argha Das <arghadas@Arghas-MacBook-Air.local>
2023-02-10 18:14:40 +0530

    joi validation schemes added for password, signin, signup and also JoiValidation decorator created

---
+++ src/shared/globals/decorators/joi-validation.decorator.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { JoiRequestValidationError } from '@global/helpers/error-handler';
+import {Request} from 'express';
+import {ObjectSchema} from 'joi';
+
+type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
+
+export function joiValidation(schema: ObjectSchema): IJoiDecorator {
+  return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    // ? args will contains [req, res, next]
+    descriptor.value = async function(...args: any[]) {
+      const req: Request = args[0];
+
+      // ? validating req.body
+      const {error} = await Promise.resolve(schema.validate(req.body));
+      if(error?.details) {
+        throw new JoiRequestValidationError(error.details[0].message);
+      }
+
+      // ? if NO error, we are resetting args value to descriptor.value
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
